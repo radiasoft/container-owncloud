4c9e6f0d69a28850f2a89d2cd4819472c1bebaf3
robnagler <github@q33.us>
2018-04-17 01:36:36 +0000

    fixups

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -2,10 +2,18 @@
 build_image_base=owncloud/server:10.0.7
 build_is_public=1
 build_simply=1
-build_docker_cmd='[]'
 build_dockerfile_aux='ENTRYPOINT []'
 
 build_as_root() {
     cd "$build_guest_conf"
     build_create_run_user
+    # hardwires the ports, and hardwired include
+    install -m 444 /dev/null /etc/apache2/ports.conf
+    install -m 555 /dev/stdin /usr/local/bin/occ <<'EOF'
+#!/bin/bash
+set -euo pipefail
+cd /var/www/owncloud
+exec php /var/www/owncloud/occ "$@"
+EOF
+    chown -R "$build_run_user:$build_run_user" /var/www/owncloud
 }
